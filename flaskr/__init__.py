b9ece35a57708fd2c6eb8565c7e52285e2c9db07
gao <gaojingru.cn@gmail.com>
2025-08-30 19:19:34 +0800

    bugfix,add style css

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -2,7 +2,7 @@ import os
 from flask import Flask
 
 
-def create_app():
+def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     print(os.path.join(app.instance_path, 'flaskr.sqlite'))
     app.config.update(SECRET_KEY='dev', DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'))
--- flaskr/db.py
+++ flaskr/db.py
@@ -1,13 +1,18 @@
 import sqlite3
 from datetime import datetime
-from flask import current_app, g
+
 import click
+from flask import current_app, g
 
 
 def get_db():
     if 'db' not in g:
-        g.db = sqlite3.connect(current_app.config['DATABASE'], detect_types=sqlite3.PARSE_DECLTYPES)
+        g.db = sqlite3.connect(
+            current_app.config['DATABASE'],
+            detect_types=sqlite3.PARSE_DECLTYPES
+        )
         g.db.row_factory = sqlite3.Row
+
     return g.db
 
 
@@ -20,6 +25,7 @@ def close_db(e=None):
 
 def init_db():
     db = get_db()
+
     with current_app.open_resource('schema.sql') as f:
         db.executescript(f.read().decode('utf8'))
 
@@ -28,10 +34,12 @@ def init_db():
 def init_db_command():
     """Clear the existing data and create new tables."""
     init_db()
-    click.echo("Initialized the database.")
+    click.echo('Initialized the database.')
 
 
-sqlite3.register_converter("timestamp", lambda v: datetime.fromisoformat(v.decode()))
+sqlite3.register_converter(
+    "timestamp", lambda v: datetime.fromisoformat(v.decode())
+)
 
 
 def init_app(app):
