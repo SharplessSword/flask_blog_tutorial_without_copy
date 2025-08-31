4da3da233e3cb957fedef1068a36f7b85d231973
gao <gaojingru.cn@gmail.com>
2025-08-31 19:20:23 +0800

    add part of blog code

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -14,8 +14,10 @@ def create_app(test_config=None):
     @app.route('/hello')
     def hello():
         return "hello, world"
-    from . import db, auth
+    from . import db, auth, blog
     db.init_app(app)
     app.register_blueprint(auth.bp)
+    app.register_blueprint(blog.bp)
+    app.add_url_rule('/', endpoint='index')
 
     return app
